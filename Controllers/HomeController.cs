43f0642028fab27fe1b8e279135c82727ebec511
silviumarinca <36280325+silviumarinca@users.noreply.github.com>
2022-05-10 17:25:16 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ParsingReport.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ReportViewer viewer = new ReportViewer();
+            viewer.LocalReport.ReportPath = Server.MapPath("~/App_Data/Test.rdl");
+            viewer.ProcessingMode = ProcessingMode.Local; 
+            viewer.Width   = 700;
+            viewer.Height = 600;
+            ViewBag.ReportViewer = viewer;
+            return View();
+        }
+
+      
+    }
+}
